10164c168b2d5bcaa79702e0b220e05abb5448ef
Mark Stevenson <markjamesstevenson@gmail.com>
2017-04-04 22:06:59 +0100

    initial stuff

--- dsChallenge.py
+++ dsChallenge.py
@@ -1,32 +1,30 @@
 import json
 from os import listdir
 from os.path import isfile, join
+import singleDataReport
 
 class loadData():
 
     def __init__(self, direct):
+        self.directory = direct
         self.allJSON = [files for files in listdir(direct)]
+        self.allReports = []
 
     def returnData(self):
-	self.
-	for singleFile in self.allJSON:
-	    myFile = json.loads(open(singleFile).read())
+        for singleFile in self.allJSON:
+            myFile = json.loads(open(self.directory + "/" + singleFile).read())
             entries = myFile['TrainingData']
-	    for item in entries:
-                publishDate = entries['webPublicationDate'] 
-		topics = entries['topics']
-		bodyText = entries['bodyText']
-	return(publishDate, topics, bodyText)
+            for item in entries:
+                publishDate = entries[item]['webPublicationDate'] 
+                topics = entries[item]['topics']
+                bodyText = entries[item]['bodyText']
+                report = singleDataReport.singleDataReport(publishDate, topics, bodyText)
+                self.allReports.append(report)
+        return self.allReports
+        
 
 
-main ()
-
-for loop...
- loadData
-dict = 
-print(myFile['TrainingData']['1999a_TrainingData_00001']['bodyText'])
-print(len(myFile['TrainingData']))
-for item in myFile['TrainingData']:
-    print(item)
-
-
+load = loadData("testData")
+result = load.returnData()
+for report in result:
+    print report.bodyText
---
+++ singleDataReport.py
@@ -0,0 +1,5 @@
+class singleDataReport():
+    def __init__(self, publishDate, topics, bodyText):
+        self.publishDate = publishDate
+        self.topics = topics
+        self.bodyText = bodyText

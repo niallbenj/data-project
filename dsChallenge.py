6f88be5aa089fde01c234a54cf410d47705b94cb
Markoprolo <markjamesstevenson@gmail.com>
2017-04-23 19:28:40 +0100

    Tidy up the naming and add gitignore

--- dsChallenge.py
+++ dsChallenge.py
@@ -5,19 +5,19 @@ import singleDataReport
 
 class loadData():
 
-    def __init__(self, direct):
-        self.directory = direct
-        self.allJSON = [files for files in listdir(direct)]
+    def __init__(self, directory):
+        self.directory = directory
+        self.allJSONFiles = [files for files in listdir(directory)]
         self.allReports = []
 
-    def returnData(self):
-        for singleFile in self.allJSON:
-            myFile = json.loads(open(self.directory + "/" + singleFile).read())
-            entries = myFile['TrainingData']
-            for item in entries:
-                publishDate = entries[item]['webPublicationDate'] 
-                topics = entries[item]['topics']
-                bodyText = entries[item]['bodyText']
+    def getAllReports(self):
+        for file in self.allJSONFiles:
+            myFile = json.loads(open(self.directory + "/" + file).read())
+            individualReport = myFile['TrainingData']
+            for item in individualReport:
+                publishDate = individualReport[item]['webPublicationDate'] 
+                topics = individualReport[item]['topics']
+                bodyText = individualReport[item]['bodyText']
                 report = singleDataReport.singleDataReport(publishDate, topics, bodyText)
                 self.allReports.append(report)
         return self.allReports
@@ -25,6 +25,6 @@ class loadData():
 
 
 load = loadData("testData")
-result = load.returnData()
+result = load.getAllReports()
 for report in result:
-    print report.bodyText
+    print(report.bodyText)

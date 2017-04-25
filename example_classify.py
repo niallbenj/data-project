bd19b6051a84431cbc2a06f6b5152b1644cfb6e9
Niall Tyndall <niall.tyndall@cyberlytic.com>
2017-04-25 22:12:03 +0100

    an example for classification

---
+++ example_classify.py
@@ -0,0 +1,25 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+from scipy.sparse.csr import csr_matrix
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.linear_model import SGDClassifier
+import sys
+
+# ~~ Example 1 ~~
+corpus = ['row, row, row your boat','whatever floats your boat']  
+test_time = ['your boat floats really well']
+
+tf = TfidfVectorizer(input='content', analyzer='word', ngram_range=(1,1),
+                     min_df = 0, sublinear_tf=True)
+
+# feature_names = tf.get_feature_names()
+tfidf_matrix = tf.fit_transform(corpus)
+test_stuff = tf.fit_transform(test_time)
+labels = np.array([[0, 1], [1, 0]])
+classifier = OneVsRestClassifier(SGDClassifier()).fit(tfidf_matrix, labels)
+y_pred = classifier.predict(test_stuff)
+print("check result!")
+print(y_pred)
+sys.exit()
+

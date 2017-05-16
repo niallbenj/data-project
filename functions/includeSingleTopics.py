bbf9083e1ccfff0171c4c5fee233f63c639f2556
Niall Tyndall <niall.tyndall@cyberlytic.com>
2017-05-16 23:33:17 +0100

    dont include if its already theregit add functions/includeSingleTopics.py  only append...

--- functions/includeSingleTopics.py
+++ functions/includeSingleTopics.py
@@ -2,6 +2,6 @@ def include(red, newLabels, reportName):
     allKeys = red.keys("*")
     for singleKey in allKeys:
         member = red.sismember(singleKey, reportName)
-        if member:
+        if member and (singleKey not in newLabels):
             newLabels = newLabels + (singleKey, )
     return(newLabels)
